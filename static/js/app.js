23f28aafabc6647761edb2e91aff0606b3bfcad1
SandraBotica <smbotica@gmail.com>
2023-03-17 18:41:07 +0800

    horizontal bar chart script written

--- static/js/app.js
+++ static/js/app.js
@@ -1,82 +1,68 @@
+// const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
 
-let samples = {
-    id=,
-    otu_ids=,
-    sample_values=,
-    otu_labels=
+// // samples.json Pending
+// const dataSamples = d3.json(url);
+// console.log("Data Samples: ", dataSamples);
+
+// // Fetch the JSON data and console log it
+// d3.json(url).then(function(data) {
+//   console.log(data);
+// });
+
+// Sort the data by sample_values descending
+let sortedSampleValues = data.sort((a, b) => b.sample_values - a.sample_values);
+
+// Slice the first 10 objects for plotting
+slicedData = sortedSampleValues.slice(0, 10);
+
+// Reverse the array to accommodate Plotly's defaults
+reversedData = slicedData.reverse();
+
+// Trace1 for the sample_values Data
+let trace1 = {
+  x: reversedData.map(object => object.sample_values),
+  y: reversedData.map(object => object.otu_ids),
+  text: reversedData.map(object => object.otu_labels),
+  type: "bar",
+  marker: {
+    color: 'rgba(50,171,96,0.6)',
+    line: {
+      color: 'rgba(50,171,96,1.0)',
+      width: 1
+    }
+  },
+  name: "OTUs Sample Values",
+  orientation: "h"
 };
 
-console.log(samples.id);
-console.log(samples.otu_ids);
-console.log(samples.sample_values);
-console.log(samples.otu_labels);
+// Data array
+// `data` has already been defined, so we must choose a new name here:
+let traceData = [trace1];
 
-let xsamplevaluesData = [sample_values];
-let yotuidsData = [otu_ids];
-let idData = [id];
-let otulabelsData = [otu_labels];
-
-let trace1 = {
-    x: xsamplevaluesData,
-    y: yotuidsData,
-    xaxis: 'x1',
-    yaxis: 'y1',
-    type: 'bar',
-    marker: {
-        color: 'rgba(50,171,96,0.6)',
-        line: {
-          color: 'rgba(50,171,96,1.0)',
-          width: 1
-        }
-      },
-    name: idData,
-    orientation: 'h'
-  };
-  
-  let data = [trace1];
-  
-  let layout = {
-    title: "Top 10 OTUs found in the individual",
-    xaxis1: {
-        range: [0, 278],
-        domain: [0, 0.5],
-        zeroline: false,
-        showline: false,
-        showticklabels: true,
-        showgrid: true
-      },
-      legend: {
-        x: 0.029,
-        y: 1.238,
-        font: {
-          size: 10
-        }
-      },
-      margin: {
-        l: 100,
-        r: 20,
-        t: 200,
-        b: 70
-      },
-      width: 600,
+// Apply a title to the layout
+let layout = {
+  title: "Top 10 OTUs Sample Values",
+  legend: {
+    x: 0.029,
+    y: 1.238,
+    font: {
+      size: 10
+    }
+  },
+  margin: {
+    l: 100,
+    r: 20,
+    t: 200,
+    b: 70
+  },
+  width: 600,
       height: 600,
       paper_bgcolor: 'rgb(248,248,255)',
-      plot_bgcolor: 'rgb(248,248,255)',
-      annotations: [
-        {
-          xref: 'paper',
-          yref: 'paper',
-          x: -0.2,
-          y: -0.109,
-          text: 'OECD ' + '(2015), Household savings (indicator), ' + 'Household net worth (indicator). doi: ' + '10.1787/cfc6f499-en (Accessed on 05 June 2015)',
-          showarrow: false,
-          font:{
-            family: 'Arial',
-            size: 10,
-            color: 'rgb(150,150,150)'
-          }
-        }
-      ]
-  };
-  
-  Plotly.newPlot("plot", data, layout);
+      plot_bgcolor: 'rgb(248,248,255)'
+};
+
+// Render the plot to the div tag with id "plot"
+// Note that we use `traceData` here, not `data`
+Plotly.newPlot("plot", traceData, layout);
+Plotly.newPlot("plot", [trace1], layout);
+
