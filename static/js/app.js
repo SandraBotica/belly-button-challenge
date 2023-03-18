64b2b6e439da5a30077e77afe9873f20b83a817a
SandraBotica <smbotica@gmail.com>
2023-03-18 17:08:49 +0800

    Bubble chart syntax added

--- static/js/app.js
+++ static/js/app.js
@@ -66,3 +66,34 @@ let layout = {
 Plotly.newPlot("plot", traceData, layout);
 Plotly.newPlot("plot", [trace1], layout);
 
+
+// Bubble chart
+let desired_maximum_marker_size = 40;
+let size = [];
+let trace2 = {
+  x: [data.map(object => object.otu_ids)],
+  y: [data.map(object => object.sample_values)],
+  text: [data.map(object => object.otu_labels)],
+  // text: ['A</br>size: 40</br>sixeref: 1.25', 'B</br>size: 60</br>sixeref: 1.25', 'C</br>size: 80</br>sixeref: 1.25', 'D</br>size: 100</br>sixeref: 1.25'],
+  mode: 'markers',
+  marker: {
+    size: size,
+    //set 'sizeref' to an 'ideal' size given by the formula sizeref = 2. * max(array_of_size_values) / (desired_maximum_marker_size ** 2)
+    sizeref: 2.0 * Math.max(data.map(object => object.sample_values)) / (desired_maximum_marker_size**2),
+    sizemode: 'area',
+    color: [data.map(object => object.otu_ids)]
+    // color: ['rgb(93, 164, 214)', 'rgb(255, 144, 14)',  'rgb(44, 160, 101)', 'rgb(255, 65, 54)']
+  }
+};
+
+let bubbleData = [trace2];
+
+let bubbleLayout = {
+  title: `Bubble Chart of Sample Values for OTU_id: ${data.map(object => object.otu_ids)}`,
+  showlegend: false,
+  height: 600,
+  width: 600
+};
+
+Plotly.newPlot('myDiv', bubbleData, bubbleLayout);
+
